c028188da7fc43cf480a3140dd5e02d2e0964d9c
Beanxxbot <btdrape@gmail.com>
2014-09-13 16:09:29 -0500

    v0.1.2.0 -            New Blocks                --Machine Casing                --Machine Casing Slab                --Nugget Dirt (Broken)            New Items                --Infused Hoe Lvl 1 now makes Nugget Dirt                --All Dirt Crusted Tiers                --Biologic crystal with 3D ITEM RENDER!!! (Broken Texture)            MANY MORE CHANGES~!

--- src/main/java/com/beanbot/beancraft/item/ItemBioCrystal.java
+++ src/main/java/com/beanbot/beancraft/item/ItemBioCrystal.java
@@ -1,6 +1,9 @@
 package com.beanbot.beancraft.item;
 
 import com.beanbot.beancraft.creativetab.CreativeTabBC;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 
 public class ItemBioCrystal extends ItemBC
 {
--- src/main/java/com/beanbot/beancraft/render/ItemRendererBioCrystal.java
+++ src/main/java/com/beanbot/beancraft/render/ItemRendererBioCrystal.java
@@ -2,7 +2,11 @@ package com.beanbot.beancraft.render;
 
 import com.beanbot.beancraft.model.ModelBioCrystal;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
@@ -11,7 +15,7 @@ import org.lwjgl.opengl.GL11;
 public class ItemRendererBioCrystal implements IItemRenderer
 {
     protected ModelBioCrystal modelBioCrystal;
-    ResourceLocation bioCrystalTexture = new ResourceLocation("beancraft/assets/models/bioCrystal.png");
+    ResourceLocation bioCrystalTexture = new ResourceLocation("beancraft", "/textures/models/bioCrystal.png");
 
     public ItemRendererBioCrystal()
     {
@@ -48,19 +52,29 @@ public class ItemRendererBioCrystal implements IItemRenderer
                 Minecraft.getMinecraft().renderEngine.bindTexture(bioCrystalTexture);
 
                 float scale = 1.2F;
-
                 GL11.glScalef(scale, scale, scale);
 
                 GL11.glRotatef(0F, 0.0F, 0.0F, 0.0F);
 
-                GL11.glTranslatef(0.65F, 0.35F, 0.0F);
+                GL11.glTranslatef(0.55F, 0.35F, 0.0F);
 
                 modelBioCrystal.render((Entity)data[1], 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
 
                 GL11.glPopMatrix();
             }
             case EQUIPPED_FIRST_PERSON:
-                break;
+            {
+                GL11.glPushMatrix();
+
+                Minecraft.getMinecraft().renderEngine.bindTexture(bioCrystalTexture);
+
+                float scale = 1.2F;
+                GL11.glScalef(scale, scale, scale);
+
+                modelBioCrystal.render((Entity)data[1], 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F);
+
+                GL11.glPopMatrix();
+            }
             case INVENTORY:
                 break;
             case FIRST_PERSON_MAP:
